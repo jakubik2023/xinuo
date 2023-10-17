296352682fbff581c0d3b1815ca9c7763c594e3c
an <d_1206@qq.com>
2023-10-17 17:09:55 +0800

    update code

--- Xinuo.py
+++ Xinuo.py
@@ -34,33 +34,39 @@ class Xinuo(Plugin):
             self.conf = super().load_config()
             self.authorization = self.conf["linkai_authorization"]
             self.cookie = self.conf["linkai_cookie"]
+            print("[Xinuo] inited")
         except:
             raise self.handle_error(e, "[Xinuo] init failed, ignore ")
-        print("[Xinuo] inited")
+
 
     def on_handle_context(self, e_context: EventContext):
         content = e_context["context"].content
         if content == "linkai签到":
+            msg = self.linkai_sign_in()
+            reply = Reply()  # 创建回复消息对象
+            reply.type = ReplyType.TEXT  # 设置回复消息的类型为文本
+            reply.content = "linkai签到\n"  # 设置回复消息的内容
+            reply.content += f"{msg}"
+            e_context["reply"] = reply
+            e_context.action = EventAction.BREAK_PASS
+        elif content == "linkai积分":
+            msg = self.linkai_balance()
+            reply = Reply()
+            reply.type = ReplyType.TEXT
+            reply.content = "linkai积分\n"
+            reply.content += f"{msg}"
+            e_context["reply"] = reply
+            e_context.action = EventAction.BREAK_PASS
 
-            msg = self.sign_in()
-            if msg:
-                reply = Reply()  # 创建回复消息对象
-                reply.type = ReplyType.TEXT  # 设置回复消息的类型为文本
-                reply.content = "linkai签到\n"  # 设置回复消息的内容
-
-                reply.content += f"{msg}"
-
-                e_context["reply"] = reply
-                e_context.action = EventAction.BREAK_PASS
-            else:
-                print("ERROR: Data not found in response")
 
     def get_help_text(self, **kwargs):
-        help_text = "输入 'linkai签到'， 将进行linkai每日积分签到操作\n"
+        help_text = "发送关键词执行对应操作\n"
+        help_text += "输入 'linkai签到'， 将进行linkai每日积分签到操作\n"
+        help_text += "输入 'linkai积分'， 将进行linkai积分获取操作\n"
         return help_text
 
 
-    def sign_in(self):
+    def linkai_sign_in(self):
         msg = ""
         try:
             url = "https://chat.link-ai.tech/api/chat/web/app/user/sign/in"
@@ -81,14 +87,6 @@ class Xinuo(Plugin):
               'sec-ch-ua-platform': '"Linux"'
             }
             response = requests.request("GET", url, headers=headers, data=payload)
-            """
-            {
-             "success": false,
-             "code": 834,
-             "message": "今日已签到，请明日再来！",
-             "data": null
-            }
-            """
             if response.status_code == 200:
                 res_json = response.json()
                 if res_json.get("code") == 200:
@@ -98,11 +96,51 @@ class Xinuo(Plugin):
                 else:
                     message = res_json.get("message")
                     msg = f"linkai签到失败:{message}"
-                    logger.info("linkai签到失败 req content: {}".format(response.text))
+                    logger.info("linkai签到失败 req content:{}".format(response.text))
             else:
                 r_code = response.status_code
                 msg = f"linkai签到失败 response status_code:{r_code}"
                 logger.info(msg)
         except:
             msg = "linkai签到失败 服务器内部错误"
+        return msg
+
+    def linkai_balance(self):
+        msg = ""
+        try:
+            url = "https://chat.link-ai.tech/api/chat/web/app/user/get/balance"
+            payload = {}
+            headers = {
+              'Accept': 'application/json, text/plain, */*',
+              'Accept-Language': 'zh-CN,zh;q=0.9',
+              'Authorization': self.authorization,
+              'Connection': 'keep-alive',
+              'Host': 'chat.link-ai.tech',
+              'Cookie': self.cookie,
+              'Referer': 'https://chat.link-ai.tech/console/account',
+              'Sec-Fetch-Dest': 'empty',
+              'Sec-Fetch-Mode': 'cors',
+              'Sec-Fetch-Site': 'same-origin',
+              'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+              'sec-ch-ua': '"Chromium";v="117", "Not;A=Brand";v="8"',
+              'sec-ch-ua-mobile': '?0',
+              'sec-ch-ua-platform': '"Linux"'
+            }
+            response = requests.request("GET", url, headers=headers, data=payload)
+            if response.status_code == 200:
+                res_json = response.json()
+                if res_json.get("code") == 200:
+                    score = res_json.get("data").get("score")
+                    msg = f"linkai总积分:{score}"
+                    logger.info(msg)
+                else:
+                    message = res_json.get("message")
+                    msg = f"linkai获取积分失败:{message}"
+                    logger.info("linkai获取积分失败 req content:{}".format(response.text))
+            else:
+                r_code = response.status_code
+                msg = f"linkai获取积分失败 response status_code:{r_code}"
+                logger.info(msg)
+        except:
+            msg = "linkai获取积分失败 服务器内部错误"
         return msg
